6ce42e8b9484ba583c0c1b66ed02f0a0ca844543
Cameron <cameronadevereaux@gmail.com>
2023-08-17 17:37:25 -0700

    Added enemy movement

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
+use bevy::prelude::*;
+
 pub mod actor;
 pub mod camera_controls;
 pub mod fov;
 pub mod level_generation;
 pub mod sprite_atlas;
+
+
+pub fn world_to_map_index(transform: &Transform) -> (usize, usize) {
+    (transform.translation.x as usize / 12, transform.translation.y as usize / 12)
+}
